db37bcd66d5f6c5e7a997c412d6e2c1361397b60
Kishan N <kishann380@gmail.com>
2018-03-15 23:36:44 +0530

    Added Exception.

--- TcpHolePunchingServer/CommunicationLayer.cs
+++ TcpHolePunchingServer/CommunicationLayer.cs
@@ -24,7 +24,7 @@ namespace TcpHolePunchingServer
             }
             catch (Exception e)
             {
-                Console.WriteLine("Exception ", e);
+                Console.WriteLine("Exception {0}", e);
             }
         }
 
